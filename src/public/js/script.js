9392ee4cd4d90d4c2eaec1e97fd77941e253d027
Jorge Munera <gerjo9211@hotmail.com>
2025-05-11 12:19:49 -0500

    asignando el usuario orrecto tomando datos desde la cookie, borrando el value del input del mensaje

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -4,14 +4,16 @@ const send = document.querySelector("#send-message");
 const allMessages = document.querySelector("#all-messages");
 
 send.addEventListener("click", () => {
-    const message = document.querySelector("#message").value;
-    socket.emit("message", message);
+    const message = document.querySelector("#message");
+    socket.emit("message", message.value);
+
+    message.value = "";
 })
 
 socket.on("message", ({user, message}) => {
     // esta es una manera mas rapida de crear elementos html, sin tener que crear uno por uno, asignarle la clase y luego ir metiendo cada elemento en su contenedor.
     // de esta manera lo hacemos mas rapido
-    const msg = document.createRange().createContextualFragment(
+    const msg = document.createRange().createContextualFragment(/*html*/
     `
         <div class="message">
             <div class="image-container">
--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -4,9 +4,14 @@ module.exports = (httpServer) => {
 
     io.on("connection", socket => {
         console.log(`sokcet id conectado ${socket.id}`);
+
+        // vamos a obtener la informaion de la cookie
+        const cookie = socket.handshake.headers.cookie;
+        const user = cookie.split("=").pop();
+
         socket.on("message", message => {
             io.emit("message", {
-                user: "jorgito",
+                user,
                 message
             });
         })
